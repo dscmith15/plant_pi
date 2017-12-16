ea0f81d64463590f739651cb24121f450a088cd1
dscmith15 <Smithdc15@gmail.com>
2017-12-16 15:43:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var io = require('socket.io')(http) //require socket.io module and pass the http
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
-  fs.readFile(__dirname + '/ang.html', function(err, data) { //read file ang.html in public folder
+  fs.readFile(__dirname + '/index.html', function(err, data) { //read file index.html in public folder
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
       return res.end("404 Not Found");
     } 
     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from ang.html
+    res.write(data); //write data from index.html
     return res.end();
   });
 }
