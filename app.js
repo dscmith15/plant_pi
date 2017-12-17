474a88b25d53a42b2c8acfb7372e38a18f555a94
dscmith15 <Smithdc15@gmail.com>
2017-12-16 19:15:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var app = require('http').createServer(handler)
-var io = require('socket.io-client')('http://localhost');
+var io = require('socket.io')(app);
 var fs = require('fs');
 
 app.listen(80);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/ang.html',
+  fs.readFile(__dirname + '/index.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading ang.html');
+      return res.end('Error loading index.html');
     }
 
     res.writeHead(200);
