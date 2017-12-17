d1d96a06a2f111676f770a8cac0c58f2c242dccc
dscmith15 <Smithdc15@gmail.com>
2017-12-16 18:57:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
-var fs = require('fs'); //require filesystem module
-var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-http.listen(8080); //listen to port 8080
+app.listen(80);
 
-function handler (req, res) { //create server
-  fs.readFile(__dirname + '/index.html', function(err, data) { //read file index.html in public folder
+function handler (req, res) {
+  fs.readFile(__dirname + '/ang.html',
+  function (err, data) {
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
+      res.writeHead(500);
+      return res.end('Error loading ang.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
   });
 }
 
